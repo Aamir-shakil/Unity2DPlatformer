697342cd576e4b302907ad0aa8818446b299aff4
Aamir-shakil <aamirshakil21@gmail.com>
2026-04-16 13:33:18 +0100

    feat: progress bar

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -1,9 +1,13 @@
+using System;
 using UnityEngine;
 
 public class Gem : MonoBehaviour, IItem
 { 
+    public static event Action<int> OnGemCollect;
+    public int worth = 5;
     public void Collect()
     {
-       Destroy(gameObject);
+        OnGemCollect.Invoke(worth);
+        Destroy(gameObject);
     }
 }
